e60e6a9ff049287bd0980847e5872982454f106b
yuanfeiasima <382810567@qq.com>
2016-09-23 11:07:20 +0800

    goto 函数

--- cat/src/func.go
+++ cat/src/func.go
@@ -21,8 +21,27 @@ func main(){
 
 	x_add_y, x_multiply_y := SumAndMultiply(x, y)
 	fmt.Printf("x_add_y:%d, x_multiply_y:%d", x_add_y, x_multiply_y)
+
+	//传递值
+	x1 := add1(x)
+	fmt.Printf("x : %d", x)
+	fmt.Printf("x1 : %d", x1)
+
+	x2 := add2(&x)
+	fmt.Printf("x : %d", x)
+	fmt.Printf("x2 : %d", x2)
 }
 
 func SumAndMultiply(A, B int)(int, int){
 	return A+B, A*B
+}
+
+func add1(a int)int{
+	a = a+1
+	return a
+}
+
+func add2(a *int) int {
+	*a = *a +1
+	 return *a
 }
